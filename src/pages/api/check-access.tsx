c83c3cbb17781f33127f28db7904457e3f3a96e8
mattiapomelli <mattiapomelli@gmail.com>
2023-04-12 22:45:24 +0100

    Add example webhook for livepeer gating

---
+++ src/pages/api/check-access.tsx
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    return res.status(200).send("OK");
+  } else {
+    return res.status(405).send("Method not allowed");
+  }
+}
