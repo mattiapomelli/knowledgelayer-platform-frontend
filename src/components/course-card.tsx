4507bc01817e7bf95dc5471094910eccf45204e3
mattiapomelli <mattiapomelli@gmail.com>
2023-04-13 17:38:49 +0100

    Refactor course card

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -1,9 +1,8 @@
+import { ethers } from "ethers";
 import Image from "next/image";
 import Link from "next/link";
-import { useAccount } from "wagmi";
 
-import { Button } from "@components/basic/button";
-import { CopyButton } from "@components/copy-button";
+import { Address } from "./address";
 
 import type { Course } from "../types/courses";
 
@@ -12,12 +11,10 @@ interface CourseCardProps {
   linkToPage?: boolean;
 }
 
-export const CourseCard = ({ course, linkToPage }: CourseCardProps) => {
-  const { address } = useAccount();
-
+export const CourseCard = ({ course }: CourseCardProps) => {
   return (
-    <div key={course.id} className="flex flex-col justify-between">
-      <div className="mb-4">
+    <Link href={`/${course.slug}`} key={course.id}>
+      <a className="flex flex-col justify-between gap-2">
         <div className="rounded-box relative h-32 overflow-hidden">
           <Image
             src={course.image}
@@ -27,33 +24,27 @@ export const CourseCard = ({ course, linkToPage }: CourseCardProps) => {
             priority
           />
         </div>
-        <Link href={`/${course.slug}`}>
-          <a className="mt-3 block text-xl font-semibold hover:opacity-80">
-            {course.title}
-          </a>
-        </Link>
-        <p className="mt-2 text-base-content/80">{course.description}</p>
-      </div>
-      {linkToPage ? (
-        <Link href={`/${course.slug}`}>
-          <a>
-            <Button block>View</Button>
-          </a>
-        </Link>
-      ) : (
-        <>
-          {address === course.seller ? (
-            <CopyButton
-              text={`${window.location.origin}/${course.slug}`}
-              className="mt-2"
-              block
-              label="Copy link"
-            />
-          ) : (
-            <Button>Watch</Button>
-          )}
-        </>
-      )}
-    </div>
+
+        <h4 className="mt-1 block text-xl font-semibold hover:opacity-80">
+          {course.title}
+        </h4>
+
+        <div className="flex items-center gap-4">
+          <span>
+            By: <Address address={course.seller} className="font-bold" />
+          </span>
+          <span>
+            Price:{" "}
+            <span className="font-bold">
+              {ethers.utils.formatEther(course.price)} MATIC
+            </span>
+          </span>
+        </div>
+
+        <p className="text-base-content/80">
+          {course.description.substring(0, 200).concat("...")}
+        </p>
+      </a>
+    </Link>
   );
 };
