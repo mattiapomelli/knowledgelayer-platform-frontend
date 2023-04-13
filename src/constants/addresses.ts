013ec5990d7ea60cf43626fc14e41d2751c4f121
mattiapomelli <mattiapomelli@gmail.com>
2023-04-13 11:55:59 +0100

    Add rpc url and address constant

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,7 +1,8 @@
-import { hardhat } from "wagmi/chains";
+import { hardhat, polygonMumbai } from "wagmi/chains";
 
 export const KNOWLEDGELAYER_COURSE_ADDRESS: {
   [chainId: number]: `0x${string}`;
 } = {
   [hardhat.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  [polygonMumbai.id]: "0x5D52a6C570e30732421E01a1c1E7Cb2c6D07a37a",
 };
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -8,3 +8,8 @@ export const EXPLORER_URL: Record<number, string> = {
 export const getAddressExplorerLink = (chainId: number, address: string) => {
   return `${EXPLORER_URL[chainId]}/address/${address}`;
 };
+
+export const RPC_URL: Record<number, string> = {
+  [polygonMumbai.id]: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  [hardhat.id]: "http://localhost:8545",
+};
