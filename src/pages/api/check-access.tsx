e8090ac282a8a148280c485f15e81d8bafc7d7bf
mattiapomelli <mattiapomelli@gmail.com>
2023-04-13 11:39:10 +0100

    Setup token gating logic in api route

--- src/pages/api/check-access.tsx
+++ src/pages/api/check-access.tsx
@@ -1,3 +1,10 @@
+import { ethers } from "ethers";
+import { hardhat } from "wagmi/chains";
+
+import { KnowledgeLayerCourseAbi } from "@abis/knowledgelayer-course";
+import { KNOWLEDGELAYER_COURSE_ADDRESS } from "constants/addresses";
+
+import type { KnowledgeLayerCourse } from "@abis/types/knowledgelayer-course";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -5,8 +12,26 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   if (req.method === "POST") {
-    return res.status(200).send("OK");
+    console.log("Body: ", req.body);
+
+    const { accessKey: userAddress } = req.body;
+
+    const provider = new ethers.providers.JsonRpcProvider(
+      "http://localhost:8545",
+    );
+    const knowledgeLayerCourse = new ethers.Contract(
+      KNOWLEDGELAYER_COURSE_ADDRESS[hardhat.id],
+      KnowledgeLayerCourseAbi,
+      provider,
+    ) as KnowledgeLayerCourse;
+
+    const balance = await knowledgeLayerCourse.balanceOf(userAddress, 1);
+    if (balance.gt(0)) {
+      return res.status(200).send({ message: "OK" });
+    }
+
+    return res.status(401).send({ message: "Unauthorized" });
   } else {
-    return res.status(405).send("Method not allowed");
+    return res.status(405).send({ message: "Method not allowed" });
   }
 }
