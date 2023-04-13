b31a686d1d5a734744aa6b8e2400c852d0456b6a
mattiapomelli <mattiapomelli@gmail.com>
2023-04-13 09:50:02 +0100

    Remove unused constant

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,11 +1,5 @@
 import { hardhat, polygonMumbai } from "wagmi/chains";
 
-if (!process.env.NEXT_PUBLIC_ALCHEMY_API_KEY) {
-  throw new Error("ALCHEMY_KEY envinronment variable must be defined");
-}
-
-export const ALCHEMY_KEY = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
-
 export const EXPLORER_URL: Record<number, string> = {
   [polygonMumbai.id]: "https://polygonscan.com",
   [hardhat.id]: "",
