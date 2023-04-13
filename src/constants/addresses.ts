e15475e76b2efd54dc5b4d373bc868a8034943a7
mattiapomelli <mattiapomelli@gmail.com>
2023-04-13 18:54:32 +0100

    Update mumbai contract address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -4,5 +4,5 @@ export const KNOWLEDGELAYER_COURSE_ADDRESS: {
   [chainId: number]: `0x${string}`;
 } = {
   [hardhat.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  [polygonMumbai.id]: "0x5D52a6C570e30732421E01a1c1E7Cb2c6D07a37a",
+  [polygonMumbai.id]: "0xF5a05B04b8a64D2a9c72e6Cd30e6Be58cA8A4c05",
 };
