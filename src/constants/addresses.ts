e01cacb1b946883ec89331898693357c6c0f9116
mattiapomelli <mattiapomelli@gmail.com>
2023-05-09 17:06:46 +0100

    Update addresses

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -4,26 +4,26 @@ export const KNOWLEDGELAYER_ID_ADDRESS: {
   [chainId: number]: `0x${string}`;
 } = {
   [hardhat.id]: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-  [polygonMumbai.id]: "0xEF76300FbbA3eA40801469409a93dD3d7a36d45B",
+  [polygonMumbai.id]: "0xaae449d58283EFa9F6b6490a9c0Aa9Cd65C2DC73",
 };
 
 export const KNOWLEDGELAYER_PLATFORM_ID_ADDRESS: {
   [chainId: number]: `0x${string}`;
 } = {
   [hardhat.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  [polygonMumbai.id]: "0x23919319dDCf7978C92cCF37875AD83686dA900B",
+  [polygonMumbai.id]: "0xf4c9B3E1c0A2C1259dB0c9D6D3EAA6404E61D550",
 };
 
 export const KNOWLEDGELAYER_COURSE_ADDRESS: {
   [chainId: number]: `0x${string}`;
 } = {
   [hardhat.id]: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
-  [polygonMumbai.id]: "0x2e7df1B8740f4be7B610Dfc6fED47754a940DDCe",
+  [polygonMumbai.id]: "0x28B43838B4CE32202f61f29A6C755B98D6abc2E8",
 };
 
 export const KNOWLEDGELAYER_ESCROW_ADDRESS: {
   [chainId: number]: `0x${string}`;
 } = {
   [hardhat.id]: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
-  [polygonMumbai.id]: "0x9D76358fb40503FFa8450F9d4Dfcf331b651d5f9",
+  [polygonMumbai.id]: "0xb79a7AAB2663fc711258fb39df823b93A8463982",
 };
