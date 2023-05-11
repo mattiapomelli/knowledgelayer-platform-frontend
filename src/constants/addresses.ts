2f6206ab2e9260823ff69785e856491d7e53bbc5
mattiapomelli <mattiapomelli@gmail.com>
2023-05-11 17:55:16 +0100

    Update testnet address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -4,5 +4,5 @@ export const KNOWLEDGELAYER_COURSE_ADDRESS: {
   [chainId: number]: `0x${string}`;
 } = {
   [hardhat.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  [polygonMumbai.id]: "0x23E911b1694A76D86000CB26EB814a57ef8ebBA5",
+  [polygonMumbai.id]: "0xe2E48da5b7ea04123bb0DCD106B295A818D63aBf",
 };
